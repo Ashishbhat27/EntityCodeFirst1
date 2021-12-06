751b8c86abb9f884380bf26d4f4b93cccd4001f2
Ashish <Ashish@LAPTOP-GNF73F8V>
2021-12-06 15:16:59 +0530

    Add project files.

---
+++ EntityCodeFirst/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityCodeFirst.Models
+{
+    [Table("tblCategories")]
+    public class Category    //this is a domain class through which we are defining categories table
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ EntityCodeFirst/Models/NorthWindContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EntityCodeFirst.Models
+{
+    public class NorthWindContext : DbContext //this context class is going to communicate with the connection string which we defined in the webconfig file.
+    {
+        public NorthWindContext()
+            : base("name=NorthWindConnection")
+        {
+
+        }
+        public DbSet<Category> CategoriesTable { get; set; }
+        public DbSet<Product> ProductsTable { get; set; }
+    }
+}
